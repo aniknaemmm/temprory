fc51509b79ff97ce05b07297cfce8f7df6f384e6
Grebenko <o.grebenko@itransition.corp>
2016-04-15 20:16:04 +0300

    init

---
+++ EngineVk.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EngineVk;
+
+namespace EngineVk.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vks = new CoreVk();
+            vks.Init("+375257307554","29730511");
+           Assert.IsTrue(vks.authorize);
+        }
+    }
+}
---
+++ EngineVk/CoreVk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+
+namespace EngineVk
+{
+    public class CoreVk
+    {
+        private const int APP_ID = 5417317;
+        public bool authorize { private set; get; }
+
+        public CoreVk()
+        {
+        }
+
+        public VkApi Init(string login, string pass)
+        {
+
+            var vk = new VkApi();
+            Settings scope = Settings.Friends;
+            vk.Authorize(new ApiAuthParams
+            {
+                ApplicationId = APP_ID,
+                Login = login,
+                Password = pass,
+                Settings = scope
+            });
+            authorize = vk.IsAuthorized;
+            return vk;
+
+        }
+
+    }
+}
+
+
